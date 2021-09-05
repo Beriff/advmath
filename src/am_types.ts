3ece6af4937b191d8beb95cfc82b6ca165563c0a
Maxim Shesternin <dofite.yes@gmail.com>
2021-09-05 11:06:16 +0700

    Add several collections
    Add queue, stack, circular buffer, tree, tree node

--- src/am_types.ts
+++ src/am_types.ts
@@ -425,10 +425,40 @@ class Complex {
     
 }
 
+class Modulus {
+    public ModulusValue: Int;
+
+    constructor (modulus: Int) {
+        this.ModulusValue = modulus;
+    }
+
+    public ValueOf(num: Int): Int {
+        return new Int(num.GetValue() % this.ModulusValue.GetValue())
+    }
+
+    public Add(num1: Int, num2: Int): Int {
+        return new Int(num1.Add(num2).GetValue() % this.ModulusValue.GetValue());
+    }
+
+    public Subtract(num1: Int, num2: Int): Int {
+        return new Int(num1.Subtract(num2).GetValue() % this.ModulusValue.GetValue());
+    }
+
+    public Multiply(num1: Int, num2: Int): Int {
+        return new Int(num1.Multiply(num2).GetValue() % this.ModulusValue.GetValue());
+    }
+
+    public Divide(num1: Int, num2: Int): Int {
+        return new Int(num1.Divide(num2).GetValue() % this.ModulusValue.GetValue());
+    }
+}
+
 class Stream<T> {
     private Conveyor: T[];
+    private Buffer: StreamBuffer<T>
     public Feed(data: T): void {
         this.Conveyor.push(data);
+        this.Buffer.Sink(this.Get());
     }
     public Get(): T {
         while (this.Conveyor.length == 0) {};
@@ -436,6 +466,294 @@ class Stream<T> {
         this.Conveyor.shift();
         return to_return;
     }
+    public GetBuffer(): StreamBuffer<T> {
+        return this.Buffer;
+    }
+    public NewBuffer(buffer: StreamBuffer<T>): void {
+        this.Buffer = buffer;
+    }
 }
 
-export { Int, Float, Fraction, CompositeFraction, Imaginary, Complex, Numeric, Formattable, BoolConvertible, amNumber, Stream }
+class StreamBuffer<T> {
+    private Buffer: T[];
+    public Sink(data: T): void {
+        this.Buffer.push(data);
+    }
+    public ReadBuffer(): T[] {
+        return this.Buffer;
+    }
+}
+
+interface amCollection<T> {
+    GetLength(): Int;
+    ShallowCopyTo(array: T[], index: Int): void;
+}
+
+type cstring = amArray<string>
+
+class amArray<T> implements amCollection<T> {
+    private array: T[];
+    constructor (arr: T[]) {
+        this.array = arr;
+    } 
+    GetLength(): Int {
+        return new Int(this.array.length);
+    }
+    ShallowCopyTo(array: T[], index: Int): void {
+        let ind: number = index.GetValue();
+        for(let i of this.array) {
+            array[ind] = i;
+            ind++;
+        }
+    }
+    public static FromArray<T>(arr: T[]): amArray<T> {
+        return new amArray<T>(arr);
+    }
+    public static FromSeq<T>(...seq: T[]): amArray<T> {
+        let array: T[] = [];
+        for (let e of seq) {
+            array.push(e);
+        }
+        return new amArray<T>(array);
+    }
+    public static FromString(str: string): cstring {
+        return new amArray<string>([...str]);
+    }
+
+    public Get(index: Int): T {
+        return this.array[index.GetValue()]
+    }
+
+    public GetN(index: number): T {
+        return this.array[index];
+    }
+
+    public Set(index: Int, value: T): void {
+        this.array[index.GetValue()] = value;
+    }
+
+    public SetN(index: number, value: T): void {
+        this.array[index] = value;
+    }
+
+    public ForEach(f: (i: T) => void) {
+        for (let j of this.array) {
+            f(j);
+        }
+    }
+
+    public IndexOf(element: T): number {
+        return this.array.indexOf(element);
+    }
+
+    public Pop(): T {
+        return this.array.pop();
+    }
+
+    public RemoveAt(index: Int): void {
+        this.array.splice(index.GetValue(), 1);
+    }
+
+    public Splice(at: Int, count: Int) {
+        this.array.splice(at.GetValue(), count.GetValue());
+    }
+
+    public Reverse(): amArray<T> {
+        let new_arr: T[] = [];
+        for (let i = this.array.length; i > 0; --i) {
+            new_arr.push(this.array[i]);
+        }
+        return new amArray(new_arr);
+    }
+
+    public Interval(from: Int = new Int(0), to: Int = this.GetLength()): amArray<T> {
+        let f: number = from.GetValue()
+        let t: number = to.GetValue()
+        let arr: T[] = [];
+        for (let i = f; i < t; ++i) {
+            arr.push(this.array[i]);
+        }
+
+        return new amArray(arr);
+    }
+
+    public Filter(f: (a: T) => boolean): amArray<T> {
+        let new_arr: T[] = [];
+        for (let e of this.array) {
+            if(f(e)) {
+                new_arr.push(e);
+            }
+        }
+        return new amArray<T>(new_arr);
+    }
+
+    public Add(e: T): void {
+        this.array.push(e);
+    }
+}
+
+class Stack<T> implements amCollection<T> {
+    private Container: T[];
+    public Add(data: T) {
+        this.Container.push(data);
+    }
+    public Pop(): T {
+        if (this.Container.length > 0) {
+            return this.Container.pop();
+        } else {
+            throw new Error("Cannot pop from stack: stack is empty")
+        }
+    }
+    public Peek(): T {
+        if (this.Container.length >= 2) {
+            return this.Container[this.Container.length - 2];
+        } else {
+            throw new Error("Cannot peek at stack: stack's length less than 2");
+        }
+    }
+    public Get(): T {
+        if (this.Container.length > 0) {
+            return this.Container[this.Container.length - 1];
+        } else {
+            throw new Error("Cannot pop from stack: stack is empty")
+        }
+    }
+    public GetLength(): Int {
+        return new Int(this.Container.length);
+    }
+
+    public ShallowCopyTo(array: T[], index: Int = new Int(1)): void {
+        let ind: number = index.GetValue();
+        for(let i of this.Container) {
+            array[ind] = i;
+            ind++;
+        }
+    }
+}
+
+class Queue<T> implements amCollection<T> {
+    private Container: T[];
+
+    public Enqueue(data: T): void {
+        this.Container.push(data);
+    }
+    public Pop(): T {
+        let to_return = this.Container[0];
+        this.Container.shift();
+        return to_return;
+    }
+    public GetLength(): Int {
+        return new Int(this.Container.length);
+    }
+    public ShallowCopyTo(array: T[], index: Int = new Int(1)): void {
+        let ind: number = index.GetValue();
+        for(let i of this.Container) {
+            array[ind] = i;
+            ind++;
+        }
+    }
+}
+
+class CircularBuffer<T> implements amCollection<T> {
+    private length: Int;
+    private Container: T[];
+    private Module: Modulus
+
+    private read_index: number = 0;
+    private write_index: number = 0;
+
+    public GetLength(): Int {
+        return this.length;
+    }
+
+    constructor(size: Int) {
+        this.length = size;
+        this.Module = new Modulus(size);
+    }
+
+    public Write(data: T): void {
+        this.Container[this.write_index] = data;
+        this.write_index = this.Module.Add(new Int(this.write_index), new Int(1)).GetValue();
+        
+    }
+
+    public ReadNext(): T {
+        let data: T = this.Container[this.read_index];
+        this.write_index = this.Module.Add(new Int(this.write_index), new Int(1)).GetValue();
+        return data;
+    }
+
+    public ShallowCopyTo(array: T[], index: Int = new Int(1)): void {
+        let ind: number = index.GetValue();
+        for(let i of this.Container) {
+            array[ind] = i;
+            ind++;
+        }
+    }
+
+}
+
+class TreeError extends Error {
+    constructor(msg: string) {
+        super(msg);
+
+        Object.setPrototypeOf(this, TreeError.prototype);
+    }
+}
+
+class TreeNode<T> {
+    public Parent: TreeNode<T>;
+    public Children: TreeNode<T>[] = [];
+    public Value: T;
+
+    constructor (parent: TreeNode<T> = null, value: T) {
+        if (!(parent == null)) {
+            this.Parent = parent;
+            this.Parent.Children.push(this);
+        };
+        this.Value = value;
+        
+    }
+
+    public AddChild(child: TreeNode<T>): void {
+        this.Children.push(child);
+        child.Parent = this;
+    }
+
+    public SetParent(parent: TreeNode<T>) {
+        this.Parent = parent;
+        parent.Children.push(this);
+    }
+
+    public Prune(): void {
+        let arr = this.Parent.Children;
+        arr.splice(arr.indexOf(this), 1);
+        this.Parent = null;
+    }
+
+    public Graft(section: Tree<T>): void {
+        section.Root.SetParent(this);
+    }
+
+    public ForEach(f: (i: TreeNode<T>) => void) {
+        for (let node of this.Children) {
+            f(node);
+            node.ForEach(f);
+        }
+    }
+}
+
+class Tree<T> {
+    public Root: TreeNode<T>;
+    constructor (root: TreeNode<T>) {
+        if (root.Parent == null) {
+            this.Root = root;
+        } else {
+            throw new TreeError("Provided root node has parent.")
+        }
+    }
+}
+
+export { Int, Float, Fraction, CompositeFraction, Imaginary, Complex, 
+    Numeric, Formattable, BoolConvertible, amNumber, 
+    Stream, StreamBuffer, Stack, Queue, CircularBuffer, TreeNode, Tree }
